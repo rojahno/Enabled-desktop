736b9924c202591e8f8f51387fca498644343ad4
eskil <eskil.torland@gmail.com>
2021-03-31 10:51:19 +0200

    Centered stepper

--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -15,6 +15,7 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       justifyContent: 'center',
       flexDirection: 'column',
+      minHeight:'100vh'
 
     },
     paper: {
